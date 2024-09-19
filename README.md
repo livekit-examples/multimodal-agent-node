0715791af2d5bccd0a86d2a23b2f1cda73bb3baa
aoife cassidy <aoife@livekit.io>
2024-09-19 15:43:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# LiveKit Agents for Node.js demo
+
+to run, set the following environmental variables (or define them in code):
+- `LIVEKIT_URL`
+- `LIVEKIT_API_KEY`
+- `LIVEKIT_API_SECRET`
+- `OPENAI_API_KEY`
---
+++ src/agent.ts
@@ -0,0 +1,38 @@
+import { type JobContext, WorkerOptions, cli, defineAgent } from '@livekit/agents';
+import { OmniAssistant, defaultConversationConfig } from '@livekit/agents-plugin-openai';
+import { z } from 'zod';
+
+export default defineAgent({
+  entry: async (ctx: JobContext) => {
+    await ctx.connect();
+
+    console.log('starting assistant example agent');
+
+    const assistant = new OmniAssistant({
+      conversationConfig: {
+        ...defaultConversationConfig,
+        system_message: 'You are a helpful assistant.',
+      },
+      functions: {
+        weather: {
+          description: 'Get the weather in a location',
+          parameters: z.object({
+            location: z.string().describe('The location to get the weather for'),
+          }),
+          execute: async ({ location }) =>
+            await fetch(`https://wttr.in/${location}?format=%C+%t`)
+              .then((data) => data.text())
+              .then((data) => `The weather in ${location} right now is ${data}.`),
+        },
+      },
+    });
+
+    await assistant.start(ctx.room);
+  },
+});
+
+// check that we're running this file and not importing functions from it
+// without this if closure, our code would start` a new Agents process on every job process.
+if (process.argv[1] === import.meta.filename) {
+  cli.runApp(new WorkerOptions({ agent: import.meta.filename }));
+}
