540013aec7405459a2d11bd85693a30d5d62bdd4
Ben Cherry <bcherry@gmail.com>
2024-10-07 12:34:10 -0700

    Add waitForParticipant (#7)
    * Add waitForParticipant
    
    * cleaner

--- src/agent.ts
+++ src/agent.ts
@@ -17,7 +17,10 @@ export default defineAgent({
   entry: async (ctx: JobContext) => {
     await ctx.connect();
 
-    console.log('starting assistant example agent');
+    console.log('waiting for participant');
+    const participant = await ctx.waitForParticipant();
+
+    console.log(`starting assistant example agent for ${participant.identity}`);
 
     const model = new openai.realtime.RealtimeModel({
       instructions: 'You are a helpful assistant.',
@@ -42,7 +45,7 @@ export default defineAgent({
     });
 
     const session = await agent
-      .start(ctx.room)
+      .start(ctx.room, participant)
       .then((session) => session as openai.realtime.RealtimeSession);
 
     session.conversation.item.create({
